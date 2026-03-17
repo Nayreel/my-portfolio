5d1c8050a5f6bec32ab94f2709235559442c7b96
Nayreel <leeryan307@gmail.com>
2026-03-17 17:40:21 +0800

    added new projects & fixed its layout into grid for fully responsiveness

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -13,52 +13,69 @@ const Projects = () => {
         <span className="text-blue-600"> hard work</span>
       </h1>
 
-      <div className="project-container flex text-white flex-wrap items-center justify-center gap-40 lg:px-7 xl:px-32 mt-28 lg:mt-14">
+      {/* ✅ GRID */}
+      <div className="project-container mt-20 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
         {projects.map((project) => (
-          <div
-            key={project.id}
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
-          >
-            <CardContainer className="inter-var flex justify-between w-full">
-              <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-[#10132E] dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+          <div key={project.id} className="w-full h-full flex items-stretch">
+            <CardContainer className="w-full h-full">
+              <CardBody
+                className="
+                  h-full
+                  flex flex-col justify-between
+                  bg-gray-50
+                  dark:bg-[#10132E]
+                  border border-black/[0.1]
+                  dark:border-white/[0.2]
+                  rounded-xl
+                  p-6
+                  w-full
+                "
+              >
+                {/* TITLE */}
                 <CardItem
                   translateZ="50"
                   className="text-xl font-bold text-neutral-600 dark:text-white"
                 >
                   {project.title}
                 </CardItem>
+
+                {/* DESCRIPTION */}
                 <CardItem
                   as="p"
                   translateZ="60"
-                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                  className="text-neutral-500 text-sm mt-2 dark:text-neutral-300"
                 >
                   {project.des}
                 </CardItem>
+
+                {/* IMAGE */}
                 <CardItem translateZ="100" className="w-full mt-4">
                   <Image
                     src={project.img}
-                    height="1000"
-                    width="1000"
-                    className="h-60 w-full object-contain rounded-xl group-hover/card:shadow-xl"
+                    height={1000}
+                    width={1000}
+                    className="h-52 w-full object-contain rounded-xl"
                     alt="project"
                   />
                 </CardItem>
+
+                {/* ICONS */}
                 <CardItem translateZ="100" className="w-full mt-4">
-                  <div className="w-full flex justify-center items-center">
+                  <div className="flex justify-center items-center gap-2 flex-wrap">
                     {project.iconLists.map((icon, i) => (
-                      <div
-                      key={i}
-                      className="w-full rounded-full flex justify-center items-center"
-                      style={{
-                        transform: `translateX(-${5 * i + 2}px)`,
-                      }}
-                    >
-                      <Image src={icon} width='100' height='100' alt="icon5" className="p-2 h-14" />
-                    </div>
+                      <Image
+                        key={i}
+                        src={icon}
+                        width={40}
+                        height={40}
+                        alt="icon"
+                        className="h-10 w-auto"
+                      />
                     ))}
                   </div>
                 </CardItem>
 
+                {/* LINK */}
                 <div className="flex justify-between items-center">
                   <CardItem
                     translateZ={20}
